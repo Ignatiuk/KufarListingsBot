584b6813a836613787d93de63b2f4021c7a085a9
Alexander <54143543+merikovich@users.noreply.github.com>
2021-07-06 13:25:08 +0300

    feat(index): cron job & monitoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
-const Slimbot = require('slimbot');
-const bot = new Slimbot('1701941027:AAHCUlRmJaaUsWh6tawYDQhbGbzUKKyntTw');
+const { Telegraf } = require('telegraf')
+const { CronJob } = require('cron');
+const config = require('../config/index.js');
 
-bot.on('message', message => {
-  bot.sendMessage(message.chat.id, 'Привет!');
-});
+const bot = new Telegraf(config.BOT_KEY);
+const KufarService = require('./helpers/kufar.js');
 
-bot.startPolling();
+let previousListing = {};
+
+async function parse() {
+  const listings = await KufarService.getListByString('macbook');
+  const newListings = [];
+
+  if (previousListing.title) {
+    for (let i = 0; i < listings.length; i++) {
+      const listing = listings[i];
+  
+      if (
+        listing.id !== previousListing.id &&
+        !listing.title.toLowerCase().includes('куплю')
+      ) {
+        newListings.push(listing);
+      } else {
+        break;
+      }
+    }
+  }
+
+  if (newListings.length) {
+    newListings.reverse().forEach(listing => {
+      const caption = `${listing.title}\n${listing.price}\n${listing.place}\n\n${listing.link}`;
+  
+      bot.telegram.sendPhoto(
+        config.CHANNEL_ID,
+        listing.image,
+        { caption },
+      );
+    });
+  }
+
+  previousListing = listings[0];
+}
+
+const job = new CronJob(
+  '*/5 * * * *',
+  async () => await parse(),
+  null,
+  true,
+  'Europe/Minsk',
+);
+
+job.start();
+bot.launch();
