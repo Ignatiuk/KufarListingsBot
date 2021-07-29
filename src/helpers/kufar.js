e5337f96a21b581e821baa77719f48a2718712aa
Alexander <54143543+merikovich@users.noreply.github.com>
2021-07-29 19:05:15 +0300

    feat(helpers/kufar): getListingPhotos function

--- src/helpers/kufar.js
+++ src/helpers/kufar.js
@@ -1,10 +1,14 @@
 const cheerio = require('cheerio');
 const got = require('got');
 
+const { paramsToQueryString } = require('../utils/utils.js');
+const LISTINGS_URL = 'https://www.kufar.by/listings?';
+
 const Service = {
-  async getListByString(queryString, count = 40) {
+  async getListByParams(params, count = 20) {
     let result = [];
-    const vgmUrl= `https://www.kufar.by/listings?ot=1&query=${queryString}`;
+    const queryString = paramsToQueryString(params);
+    const vgmUrl = LISTINGS_URL + queryString;
 
     await got(vgmUrl).then(response => {
       const $ = cheerio.load(response.body);
@@ -21,13 +25,18 @@ const Service = {
       const arr = list.map(child => {
         const $ = cheerio.load(child);
         const link = child.attribs.href;
+        let price = $(selectors.price).text();
+
+        if (price.includes('р.')) {
+          price = price.replace(' р.', '').split(' ').join('');
+        }
 
         return {
           link,
           id: link.replace('https://www.kufar.by/item/', ''),
           date: $(selectors.date).text(),
           title: $(selectors.title).text(),
-          price: $(selectors.price).text(),
+          price: price + ' рублей',
           place: $(selectors.place).text(),
           image: child.children[0].children[0].attribs['data-src'],
         };
@@ -36,6 +45,25 @@ const Service = {
       result = arr;
     }).catch(err => {
       console.log(err);
+    });
+
+    return result;
+  },
+
+  async getListingPhotos(listingUrl) {
+    let result = [];
+
+    await got(listingUrl).then(response => {
+      const $ = cheerio.load(response.body);
+      const photosData = $('.swiper-wrapper')[1].children;
+
+      const photos = photosData.map(child => {
+        const photoData = child.children[0].children[0];
+
+        return photoData.attribs.src;
+      })
+
+      result = photos
     });
 
     return result;
