d8df0d5382d0e0303be7a3e767a85578b981889c
Alexander <54143543+merikovich@users.noreply.github.com>
2021-07-29 19:03:49 +0300

    feat(logic/cron): cron jobs

---
+++ src/logic/cron.js
@@ -0,0 +1,75 @@
+const { CronJob } = require('cron');
+const bot = require('./telegram.js');
+
+const KufarService = require('../helpers/kufar.js');
+const Request = require('../models/request.js');
+
+async function parse({ query, userId, lastListing }) {
+  const listings = await KufarService.getListByParams({ query, onlyInTitles: true }, 10);
+  const newListings = [];
+
+  if (lastListing) {
+    for (let i = 0; i < listings.length; i++) {
+      const listing = listings[i];
+  
+      if (
+        listing.id !== lastListing.id
+        && !listing.title.toLowerCase().includes('куплю')
+      ) {
+        newListings.push(listing);
+      } else {
+        break;
+      }
+    }
+  } else {
+    newListings.push(listings[0]);
+  }
+
+  if (newListings.length) {
+    newListings.reverse().forEach(async listing => {
+      try {
+        const caption = `*${listing.title}*\n\n${listing.price}\n${listing.place}\n\n${listing.link}`;
+        const photos = await KufarService.getListingPhotos(listing.link);
+
+        const media = photos.slice(0, 10).map((photoUrl, i) => ({
+          type: 'photo',
+          media: { url: photoUrl },
+          caption: i === 0 ? caption : '',
+          parse_mode: 'Markdown',
+        }));
+
+        await bot.telegram.sendMediaGroup(userId, media);
+      } catch (e) {
+        if (e.response.description.includes('bot was blocked by the user')) {
+          await Request.deleteOne({ userId });
+        }
+
+        console.error(e);
+      }
+    });
+  }
+
+  await Request.updateOne({ userId }, { lastListing: listings[0] });
+}
+
+async function checkRequests() {
+  const requests = await Request.find({});
+
+  if (!requests.length) {
+    return;
+  }
+
+  requests.forEach(async req => {
+    await parse(req);
+  });
+}
+
+const job = new CronJob(
+  '*/3 * * * *',
+  async () => await checkRequests(),
+  null,
+  true,
+  'Europe/Minsk',
+);
+
+job.start();
