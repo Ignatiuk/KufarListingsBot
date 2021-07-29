438dcc5a183c785737a807b987cf640c725f3eb6
Alexander <54143543+merikovich@users.noreply.github.com>
2021-07-29 19:04:33 +0300

    refactor(index): moved logic to several files

--- src/index.js
+++ src/index.js
@@ -1,53 +1,8 @@
-const { Telegraf } = require('telegraf')
-const { CronJob } = require('cron');
-const config = require('../config/index.js');
+// database
+require('./logic/db.js');
 
-const bot = new Telegraf(config.BOT_KEY);
-const KufarService = require('./helpers/kufar.js');
+// telegram bot
+require('./logic/telegram.js');
 
-let previousListing = {};
-
-async function parse() {
-  const listings = await KufarService.getListByString('macbook');
-  const newListings = [];
-
-  if (previousListing.title) {
-    for (let i = 0; i < listings.length; i++) {
-      const listing = listings[i];
-  
-      if (
-        listing.id !== previousListing.id &&
-        !listing.title.toLowerCase().includes('куплю')
-      ) {
-        newListings.push(listing);
-      } else {
-        break;
-      }
-    }
-  }
-
-  if (newListings.length) {
-    newListings.reverse().forEach(listing => {
-      const caption = `${listing.title}\n${listing.price}\n${listing.place}\n\n${listing.link}`;
-  
-      bot.telegram.sendPhoto(
-        config.CHANNEL_ID,
-        listing.image,
-        { caption },
-      );
-    });
-  }
-
-  previousListing = listings[0];
-}
-
-const job = new CronJob(
-  '*/5 * * * *',
-  async () => await parse(),
-  null,
-  true,
-  'Europe/Minsk',
-);
-
-job.start();
-bot.launch();
+// cron jobs
+require('./logic/cron.js');
